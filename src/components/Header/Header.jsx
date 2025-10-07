8a03e620ccbf0df2631e23950a33912c01cff1a0
nicolasfernandosap <nicolasfernandosap@gmail.com>
2025-10-07 16:45:09 -0300

    Aplicando botão de icone para responsividade em smartphones

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import './Header.css';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
-      <div className="nav-wrapper"> {/* <- Adicionado aqui */}
-        <nav className="navbar">
+      <div className="nav-wrapper">
+        <div className="menu-icon" onClick={toggleMenu}>
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </div>
+
+        <nav className={`navbar ${menuOpen ? 'active' : ''}`}>
           <ul className="nav-links">
-            <li><Link to="/">Página Inicial</Link></li>
-            <li><Link to="/auxilio">Auxílio Lixo Eletrônico</Link></li>
-            <li><Link to="/pontos">Pontos Autorizados</Link></li>
-            <li><Link to="/contato">Contato</Link></li>
-            <li><Link to="/cadastro">Cadastre-se</Link></li>
+            <li><Link to="/" onClick={() => setMenuOpen(false)}>Página Inicial</Link></li>
+            <li><Link to="/auxilio" onClick={() => setMenuOpen(false)}>Auxílio Lixo Eletrônico</Link></li>
+            <li><Link to="/pontos" onClick={() => setMenuOpen(false)}>Pontos Autorizados</Link></li>
+            <li><Link to="/contato" onClick={() => setMenuOpen(false)}>Contato</Link></li>
+            <li><Link to="/cadastro" onClick={() => setMenuOpen(false)}>Cadastre-se</Link></li>
           </ul>
         </nav>
       </div>
